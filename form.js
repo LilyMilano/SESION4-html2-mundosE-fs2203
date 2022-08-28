e395a10efcda004afbae5485b5071d9532728c90
Liliana Milano <milanoliliana129@gmail.com>
2022-08-27 22:53:55 -0400

    Added files

--- form.js
+++ form.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const form = document.forms[0];
 form.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -5,4 +6,13 @@ form.addEventListener("submit", function (event) {
     console.log(form);
     console.log(data);
 });
+=======
+const form = document.forms[0];
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let data = new FormData(form);
+    console.log(form);
+    console.log(data);
+});
+>>>>>>> 7e2ad099bef9d948f69cee42edc6bf9e67c9bcc0
 //console.log('formulario')
